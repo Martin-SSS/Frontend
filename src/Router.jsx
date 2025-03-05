6370d4dd61de640ba8dbcc6183f25d01489f87b2
Martin-SSS <158244508+Martin-SSS@users.noreply.github.com>
2025-03-05 12:12:45 -0500

    Add files via upload

--- src/Router.jsx
+++ src/Router.jsx
@@ -15,17 +15,8 @@ import {
   Calendar,
   Stream,
 } from "./scenes";
-import Worker1Page from "./pages/Worker1Page";
-import Worker2Page from "./pages/Worker2Page";
-import Worker3Page from "./pages/Worker3Page";
-import Worker4Page from "./pages/Worker4Page";
+import WorkerDetail from './pages/WorkerDetail'
 import SettingPage from "./pages/SettingPage";
-import WorkerTerminal from "./pages/WorkerTerminal";
-import WorkerPrediction from "./pages/WorkerPrediction";
-import WorkerTaskAllocation from "./pages/WorkerTaskAllocation";
-import WorkerUploadFile from "./pages/WorkerUploadFile";
-
-
 const AppRouter = () => {
   return (
     <Router>
@@ -45,34 +36,10 @@ const AppRouter = () => {
           <Route path="/geography" element={<Geography />} />
 
           {/* Worker Pages */}
-          <Route path="/worker1" element={<Worker1Page />} />
-          <Route path="/worker2" element={<Worker2Page />} />
-          <Route path="/worker3" element={<Worker3Page />} />
-          <Route path="/worker4" element={<Worker4Page />} />
+          <Route path="/:workerId" element={<WorkerDetail />} />
 
           {/* Setting Page */}
           <Route path="/settings" element={<SettingPage />} />          
-
-          {/* Worker Submenu Routes */}
-        <Route path="/worker1/terminal" element={<WorkerTerminal />} />
-        <Route path="/worker1/prediction" element={<WorkerPrediction />} />
-        <Route path="/worker1/task-allocation" element={<WorkerTaskAllocation />} />
-        <Route path="/worker1/upload-file" element={<WorkerUploadFile />} />
-
-        <Route path="/worker2/terminal" element={<WorkerTerminal />} />
-        <Route path="/worker2/prediction" element={<WorkerPrediction />} />
-        <Route path="/worker2/task-allocation" element={<WorkerTaskAllocation />} />
-        <Route path="/worker2/upload-file" element={<WorkerUploadFile />} />
-
-        <Route path="/worker3/terminal" element={<WorkerTerminal />} />
-        <Route path="/worker3/prediction" element={<WorkerPrediction />} />
-        <Route path="/worker3/task-allocation" element={<WorkerTaskAllocation />} />
-        <Route path="/worker3/upload-file" element={<WorkerUploadFile />} />
-
-        <Route path="/worker4/terminal" element={<WorkerTerminal />} />
-        <Route path="/worker4/prediction" element={<WorkerPrediction />} />
-        <Route path="/worker4/task-allocation" element={<WorkerTaskAllocation />} />
-        <Route path="/worker4/upload-file" element={<WorkerUploadFile />} />
         </Route>
       </Routes>
     </Router>
